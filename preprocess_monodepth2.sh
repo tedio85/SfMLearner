13a8acf9b3cf60c9b641ef78899200c88a5b337c
Ted Yu <tedio85@yahoo.com.tw>
2019-10-28 18:20:51 -0700

    added script for preprocess

---
+++ preprocess_monodepth2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip install joblib
+python data/prepare_train_data.py \
+--dataset_dir=/viscompfs/users/tedyu/kitti_data/kitti/ \
+--dataset_name='kitti_raw_eigen' \
+--dump_root=/viscompfs/users/tedyu/kitti_data/kitti_monodepth2/ \
+--seq_length=3   \
+--img_width=640  \
+--img_height=192 \
+--num_threads=4  
